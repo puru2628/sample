a2505c945f512b84e79f616ea37796e6ac40b580
Purushottam Singh <purushottamsingh.raj96@gmail.com>
2024-02-27 07:33:22 +0530

    sampleCodeChanges

--- C++/1. Basic I/O/lineChangeSample.cpp
+++ C++/1. Basic I/O/lineChangeSample.cpp
@@ -8,5 +8,15 @@ int main(){
     cout<<"Hello " <<x << "\n";
     cout<<"and " <<y <<" with \\n" << "\n";
     cout<<"Hello " <<x <<endl <<"and " <<y <<" with endl";
+
+
+    cout << "Hello World\t";
+    cout << "Cool World" << endl;
+
+    cout << "Hello World\\";
+    cout << "Cool World\n";
+
+    cout << "Hello World\"";
+    cout << "Cool World\"";
     return 0;
 }
